d19474b93d67bb09cde9b01744d382168c0dd035
Debora Sanchez <debora.sanchez.ga@gmail.com>
2026-07-09 09:21:02 +0200

    Add SJF Priority to ground crew and vehicles to optimize thoroughput and reformatted CSV to better show departure times.

--- main.py
+++ main.py
@@ -25,16 +25,22 @@ class Aircraft:
 
     def process_turnaround(self):
         arrival_time = self.env.now
+
+        # Calculate shortest time to implement SJF priority
+        service_time = np.random.triangular(10, 20, 40)
         
-        # Requesting resources from the simulation instance
-        with self.simulation.gates.request() as gate_req, \
-             self.simulation.ground_crew.request() as crew_req, \
-             self.simulation.service_vehicles.request() as veh_req:
+        # Requesting resources, gates are FIFO, crew and vehicles SJF
+
+        # Gete is requested first to ensure aircraft requests crew and vehicles only after it has parked
+        with self.simulation.gates.request() as gate_req:
+            yield gate_req
+
+        with self.simulation.ground_crew.request(priority=service_time) as crew_req, \
+             self.simulation.service_vehicles.request(priority=service_time) as veh_req:
             
-            yield gate_req & crew_req & veh_req
+            yield crew_req & veh_req
             
             # Servicing (Triangular Distribution)
-            service_time = np.random.triangular(10, 20, 40)
             yield self.env.timeout(service_time)
             
             # Collect metrics
@@ -42,22 +48,22 @@ class Aircraft:
 
 # Airport Simulation
 class AirportSimulation:
-    ARRIVAL_RATE = 0.8333 # estimating 40 planes in an 8 hour day (480 minutes)
+    ARRIVAL_RATE = 0.08333 # estimating 40 planes in an 8 hour day (480 minutes)
     SIMULATION_DURATION = 480 # 8 hours in minutes
 
     def __init__(self, env, arrival_rate=ARRIVAL_RATE): 
         self.env = env
         self.arrival_rate = arrival_rate
         self.gates = simpy.Resource(env, capacity=4) 
-        self.ground_crew = simpy.Resource(env, capacity=2)
-        self.service_vehicles = simpy.Resource(env, capacity=2)
+        self.ground_crew = simpy.PriorityResource(env, capacity=2)
+        self.service_vehicles = simpy.PriorityResource(env, capacity=2)
         self.metrics = []
 
     def arrival_generator(self):
         count = 1
         while True:
             # Exponential Distribution 
-            yield self.env.timeout(np.random.exponential(1.0 / self.arrival_rate))
+            yield self.env.timeout(np.random.exponential(1.0 / self.ARRIVAL_RATE))
             
             # New Aircraft
             plane = Aircraft(f"Plane {count}", self.env, self)
@@ -70,11 +76,18 @@ class AirportSimulation:
 
     def record_metrics(self, name, arrival_time, service_time):
         wait_time = self.env.now - arrival_time - service_time
+        completion_time_minutes = self.env.now
+
+        shift_start = pd.to_datetime('08:00:00')
+        actual_time = shift_start + pd.to_timedelta(completion_time_minutes, unit='m')
+        clock_time_str = actual_time.strftime('%I:%M %p')
+
+
         self.metrics.append({
             'aircraft': name,
-            'wait_time': wait_time,
-            'service_time': service_time,
-            'completion_time': self.env.now
+            'wait_time': round(wait_time, 2),          # Rounded for cleaner CSV
+            'service_time': round(service_time, 2),    # Rounded for cleaner CSV
+            'departure_clock_time': clock_time_str
         })
 
     def save_to_csv(self, filename):
